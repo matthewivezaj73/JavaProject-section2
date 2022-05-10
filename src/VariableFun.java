495f8d3b4276028d30bc12ab5e214bba7e8b8a48
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-05-09 22:45:14 -0400

    Adding projects.

--- src/VariableFun.java
+++ src/VariableFun.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class VariableFun {
+//Creating a class.
+public class VariableFun {
+    //Our main method.
+    public static void main(String[] args) {
+        //Variables should start with a lowercase letter.
+        //Setting variables.
+        //Variable declaration with the assignment (= sign) operator.
+        int my_age;
+        my_age = 19;
+        //Creating a symbolic constant, cannot be changed.
+        final int SOME_NUM = 150;
+        //Creating a variable to hold my home town.
+        String home_variable = "Shelby Township.";
+
+        //Initialization.
+        String name = "Billy";
+        //Println introduces another character (insertion line).
+        //Println displays strings and integers.
+        //Displaying the user's name and age on the screen.
+        System.out.print(name + " is " + my_age+'\n');
+        System.out.print("I live in " + home_variable + "\n");
+    }
 }
