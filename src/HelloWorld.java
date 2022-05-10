3af4867d41c9ebde30fadd0cc7e10976da8c4cb3
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-05-09 22:20:53 -0400

    Added a prinst statement.

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+//In Java, it is required that the class matches the file name.
+//A class is an entity that holds data and behaviors.
+public class HelloWorld {
+    //Our single behavior. The main method is a special method that acts as the entry method to our program.
+    public static void main(String[] args) {
+        //System is a class that we have access to.
+        //Out is a reference to an object that lives in that class that is called the standard output object.
+        System.out.println("Hello world!");
+        //Adding another print statement that says Hello Matt!
+        System.out.println("Hello Matt!");
+    }
+}
