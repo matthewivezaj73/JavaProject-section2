5b6f5b2308db2bc25c487606a87a55defedb6cf5
matthewivezaj73 <81784356+matthewivezaj73@users.noreply.github.com>
2022-05-10 21:58:48 -0400

    modified files.

--- src/ArithmeticFun.java
+++ src/ArithmeticFun.java
@@ -16,6 +16,7 @@ public class ArithmeticFun {
         //Creating variables of type int.
         int a = 10;
         int b = 15;
+        //THESE ARE BINARY OPERATORS.
         //Creating a variable to represent the sum.
         int result = a + b;
         //Creating a variable to represent the difference.
@@ -36,13 +37,27 @@ public class ArithmeticFun {
         System.out.println("The remainder is " + remainder);
         //Printing out the result.
         System.out.println("The result of the sum is: " + result);
-        //Printing out the types of arithmetic operators in Java.
-        System.out.println("The types of arithmetic operators in Java are -, +, /, *, and %.");
-        //Printing out the types of compound operators in Java.
-        System.out.println("The compound arithmetic operators in Java are +=, -=, *=, /=, and %=.");
         //Taking the result and adding 20.
         result += 20;
         //Printing out the new result.
         System.out.println("The result is now " + result);
+        //Adding one on to the result.
+        result++;
+
+        //Displaying the result.
+        System.out.println("result++ " + result);
+        //Adding one on to the result.
+        result--;
+        //THESE ARE UNARY OPERATORS.
+        //Displaying the result.
+        System.out.println("result-- " + result);
+        //Printing out the types of arithmetic operators in Java.
+        System.out.println("The types of arithmetic operators in Java are -, +, /, *, and %.");
+        //Printing out the types of compound operators in Java.
+        System.out.println("The compound arithmetic operators in Java are +=, -=, *=, /=, and %=.");
+        //MULTIPLYING PRODUCT BY TWO.
+        product *= 2;
+        //Printing out the value of the product.
+        System.out.println(product);
     }
 }
--- src/RelationalFun.java
+++ src/RelationalFun.java
@@ -1,2 +1,42 @@
-package PACKAGE_NAME;public class RelationalFun {
+//Creating a class.
+public class RelationalFun {
+    //Creating a main method
+    public static void main(String[] args)
+    {
+        //Creating a variable of type bool.
+        boolean myBool = true;
+        boolean yourBool = false;
+        //Creating variables of type int.
+        int myAge = 37;
+        int yourAge = 20;
+        int bobsAge = 20;
+        int currentAge = 27;
+        //Printing out some statements.
+        System.out.println("mybool is " + myBool);
+        System.out.println("yourBool is " + yourBool);
+        //Creating names.
+        String myName = "John";
+        String yourName = "John";
+        //Relational operations.
+        boolean ageComparison = myAge > yourAge;
+        //Printing ageComparison.
+        System.out.println("myAge > yourAge? " + ageComparison);
+        //Overwritting the previous value on ageComparison with a new one.
+        ageComparison = yourAge > bobsAge;
+        //Printing a message.
+        System.out.println("yourAge > bobsAge? " + ageComparison);
+        //Comparing the ages.
+        ageComparison = yourAge == bobsAge;
+        //Printing the comparison.
+        System.out.println("yourAge == bobsAge? " + ageComparison);
+        //Comparing my age to 21.
+        ageComparison = currentAge >= 21;
+        //Printing out the boolean.
+        System.out.println("currentAge >= 21? " + ageComparison);
+
+        //Comparing names.
+        boolean nameComparison = myName.equals(yourName);
+        //Printing out a message.
+        System.out.println("Do the names match? " + nameComparison);
+    }
 }
